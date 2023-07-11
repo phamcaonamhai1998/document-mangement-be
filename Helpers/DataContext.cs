df45760cd82e1952a580be8b2413a68cf47f1f75
Minh <mylovesnsd1998@gmail.com>
2023-07-11 16:16:26 +0700

    update
    add

---
+++ Helpers/DataContext.cs
@@ -0,0 +1,23 @@
+namespace WebApi.Helpers;
+
+using Microsoft.EntityFrameworkCore;
+using WebApi.Entities;
+
+public class DataContext : DbContext
+{
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+    private readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to sqlite database
+        options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+    }
+}
