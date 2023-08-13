5b34fe530d3818c7dad54572bbdca0af0ae179d7
Hai Pham <phamcaonamhai@gmail.com>
2023-08-13 17:52:03 +0700

    fix: remove get pwd

--- Models/Users/UserDto.cs
+++ Models/Users/UserDto.cs
@@ -10,7 +10,6 @@ public class UserDto
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
-    public string PasswordHash { get; set; }
     public string AvatarUrl { get; set; }
     public string Phone { get; set; }
     public Organization Org{ get; set; }
