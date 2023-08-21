60448bb5dddb15749b94a7a4160b62e40fb0398d
Hai Pham <phamcaonamhai@gmail.com>
2023-08-21 20:24:24 +0700

    role migration

---
+++ Migrations/20230821132342_RoleMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RoleMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DepartmentId",
+                table: "Roles",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "Roles");
+        }
+    }
+}
